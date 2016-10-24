899ad2dbff5f872cf37db436aaa1e131bab2dc98
Colin Barrett-Fox <cfox@cbtnuggets.com>
2016-10-24 11:56:48 -0700

    Linting

--- utils/generateUUID.js
+++ utils/generateUUID.js
@@ -4,11 +4,11 @@
  * @return {string} A V4 UUID.
  * @private
  */
-module.exports = function (){
+module.exports = function () {
     var d = new Date().getTime();
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c === 'x' ? r : (r&0x7|0x8)).toString(16);
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x7 | 0x8)).toString(16);
     });
 };
